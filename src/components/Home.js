d0e16e2cf1fe0dce565149f3cb55b3e3143c1a34
General Assembly <generalassembly@tests-MacBook-Air.local>
2023-01-22 22:50:42 -0500

    fixed discord app thing

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ function Home() {
                 <div className="i-am">
                     <p>I coded a very simple discord bot that asks coding questions I learned throughout my bootcamp. All you have to do is !quiz for it to ask a question</p>
                 </div>
-                <a className="resumelink" href={process.env.REACT_APP_DISCORD} target="_blank" rel="noreferrer"><button className="resumebutton">Add the Bot</button></a>
+                <a className="resumelink" href="https://discord.com/api/oauth2/authorize?client_id=1065674929640583389&permissions=274877983744&scope=bot" target="_blank" rel="noreferrer"><button className="resumebutton">Add the Bot</button></a>
             </div>
         </>
     )
