4ae2cb7829c6a216588be49de1e47bd636809324
General Assembly <generalassembly@tests-MacBook-Air.local>
2023-01-23 00:00:25 -0500

    updated text

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ function Home() {
             <div className="intro-box">
                 <h1 className="title-name">VERY SIMPLE CODING DISCORD QUIZ BOT</h1>
                 <div className="i-am">
-                    <p>I coded a very simple discord bot that asks coding questions I learned throughout my bootcamp. All you have to do is !quiz for it to ask a question</p>
+                    <p>A bot that asks true/false questions about coding stuff I learned in my bootcamp. Once you have it in your server, !quiz to get a question, and answer within 10 seconds</p>
                 </div>
                 <a className="resumelink" href="https://discord.com/api/oauth2/authorize?client_id=1065674929640583389&permissions=274877983744&scope=bot" target="_blank" rel="noreferrer"><button className="resumebutton">Add the Bot</button></a>
             </div>
